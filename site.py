4950e1aa5cd0c8c08b52c02ad7de9959fcd48837
Spencer Miller <spence.miller@gmail.com>
2015-07-22 03:01:30 -0700

    Ported the knapsack logic over to js

--- site.py
+++ site.py
@@ -5,6 +5,10 @@ from fml_knapsack import lookup
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', lookup=lookup)
+
+if __name__ == '__main__':
+    app.run()
