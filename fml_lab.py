b18021004248eb6456a77d55aa2f714ab5050d6d
Spencer Miller <spence.miller@gmail.com>
2015-08-15 18:06:07 -0700

    putting in lookup check to make sure the data is fresh

--- fml_lab.py
+++ fml_lab.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask import json
 from flask import render_template
-from fml_knapsack import lookup
+from retrieval import projections_table
+import datetime
 
 app = Flask(__name__)
 
+lookup = None
+last_lookup = None
 
 @app.route('/')
 def index():
+    global lookup, last_lookup
+    if last_lookup is None or datetime.date.today() - last_lookup >= datetime.timedelta(1):
+        last_lookup = datetime.date.today()
+        lookup = projections_table()
     return render_template('index.html', lookup=lookup)
 
 if __name__ == '__main__':
